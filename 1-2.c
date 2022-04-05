a5346bd65cfb4d2dc1f3d548ec0dc975e24e1a5e
LuYee <aulaul920106@gmail.com>
2022-04-06 00:35:27 +0800

    Create 1-2.c

---
+++ 1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("H   H     A\n");
+    printf("H   H    A A\n");
+    printf("HHHHH   A   A\n");
+    printf("H   H  AAAAAAA\n");
+    printf("H   H  A     A\n");
+}
