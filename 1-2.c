07a6512424fc42feb432e2ccfad1df97be564795
LuYee <aulaul920106@gmail.com>
2022-04-07 04:04:23 +0800

    update

--- 1-2.c
+++ 1-2.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("H   H     A\n");
-    printf("H   H    A A\n");
-    printf("HHHHH   A   A\n");
-    printf("H   H  AAAAAAA\n");
-    printf("H   H  A     A\n");
+    printf("H    H      A\n");
+    printf("H    H     A A\n");
+    printf("HHHHHH    A   A\n");
+    printf("H    H   AAAAAAA\n");
+    printf("H    H  A       A\n\n");
+
+    printf("H    H      A\n"
+           "H    H     A A\n"
+           "HHHHHH    A   A\n" 
+           "H    H   AAAAAAA\n"
+           "H    H  A       A\n");
+    return 0;
 }
