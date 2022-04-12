497b1504b826c1941ded799ef40ae32ed4a13743
LuYee <aulaul920106@gmail.com>
2022-04-12 13:27:38 +0800

    update

---
+++ 2-9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char input, output;
+    printf("Input A-Z:");
+    scanf("%c", &input);
+    output = input + 32;
+    // A = 65 a = 97 
+    printf("Output: %c\n", output);
+    return 0;
+}
